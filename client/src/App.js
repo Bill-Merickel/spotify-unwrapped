2e317924f9c5b99e52b2cdbf49b5f3a2b71fa963
Bill Merickel <billmerickel@gmail.com>
2023-07-08 17:18:37 -0400

    Optimize react and express code with npm concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,15 @@ function App() {
     const urlParams = new URLSearchParams(queryString);
     const accessToken = urlParams.get('access_token');
     const refreshToken = urlParams.get('refresh_token');
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+    }
   }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
