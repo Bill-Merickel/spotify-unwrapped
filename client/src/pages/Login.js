92771b7499064c2b73726134d2362bf0bd15e44e
Bill Merickel <billmerickel@gmail.com>
2023-07-09 23:30:41 -0400

    Add modified Heroku URL

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -24,9 +24,14 @@ const StyledLoginButton = styled.a`
   }
 `;
 
+const LOGIN_URI =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:8888/login'
+    : 'https://spotify-unwrapped-605ec8d19cc5.herokuapp.com/login';
+
 const Login = () => (
   <StyledLoginContainer>
-    <StyledLoginButton href="http://localhost:8888/login">
+    <StyledLoginButton href={LOGIN_URI}>
       Log in to Spotify
     </StyledLoginButton>
   </StyledLoginContainer>
